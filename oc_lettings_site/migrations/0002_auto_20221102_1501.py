9644f6bfc776e4df7abc91597e9908be572517d3
EmmanuelD79 <e.demeulnaere@gmail.com>
2022-11-02 18:36:20 +0100

    refactorisation

---
+++ oc_lettings_site/migrations/0002_auto_20221102_1501.py
@@ -0,0 +1,37 @@
+from django.db import migrations
+
+
+def migrate_datas(apps, schema_editor):
+    # We can't import the Person model directly as it may be a newer
+    # version than this migration expects. We use the historical version.
+
+    try:
+        OldAddress = apps.get_model('oc_lettings_site', 'Address')
+        OldLetting = apps.get_model('oc_lettings_site', 'Letting')
+    except LookupError:
+        # The old app isn't installed.
+        return
+
+    NewAddress = apps.get_model('lettings', 'Address')
+    NewAddress.objects.bulk_create(
+        NewAddress(number=old_object.number, street=old_object.street, city=old_object.city, state=old_object.state, zip_code=old_object.zip_code, country_iso_code=old_object.country_iso_code)
+        for old_object in OldAddress.objects.all()
+    )
+    
+    NewLetting = apps.get_model('lettings', 'Letting')
+    NewLetting.objects.bulk_create(
+        NewLetting(title=old_object.title, address_id=old_object.address_id)
+        for old_object in OldLetting.objects.all()
+    )
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lettings', '0001_initial'),
+        ('oc_lettings_site', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_datas),
+    ]
