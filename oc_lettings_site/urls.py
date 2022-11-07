b1ba9c41fd53e3672bd7539635482e1143e96ded
EmmanuelD79 <e.demeulnaere@gmail.com>
2022-11-07 15:02:59 +0100

    Correction linter

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,15 +3,9 @@ from django.urls import path, include
 from . import views
 
 
-
-# def trigger_error(request):
-#     '''Sentry test'''
-#     division_by_zero = 1 / 0
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
-    #path('sentry-debug/', trigger_error),
 ]
